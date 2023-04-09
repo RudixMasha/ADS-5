c5a44c6cbe7fd82ebb1388ad0d49e17bc4113e54
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-04-09 10:22:05 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,33 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[100];
+    int top;
+
+ public:
+    TStack() :top(-1) {}
+    void push(T value) {
+        if (isfull())
+            throw std::string("Full");
+        else
+            arr[++top] = value;
+    }
+    const T& pop() {
+        if (isempty())
+            throw std::string("Empty");
+        else
+            return arr[top--];
+    }
+    const T& get() {
+        return arr[top];
+    }
+    bool isempty() {
+        return top == -1;
+    }
+    bool isfull() {
+        return top == 100;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
